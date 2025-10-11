16dcf4e1a351215867d3924d53d7c36f0563dc20
Micheal Bear <Jacquelinecadybear@gmail.com>
2025-10-11 15:17:20 -0700

    word data struct, hash bucket struct, hash table struct, added, not debuged or tested

--- hash_table.c
+++ hash_table.c
@@ -3,19 +3,47 @@
 #include <string.h>
 
 //Word Data Struct (10 pts)
-  //list structure: Dont include in header file
-  //output The string representing the word itself
+  //hopefully save space by making only one word and having multiple types ref it 
+  typedef struct Word{ 
+    char str[];
+    int textFreq;
+  }Word;
 
-  //output The number of times it appears in the text
+  //tells how oft a word is aft a word, used for a list
+  typedef struct WordFreq{
+    Word w;
+    int aftFreq;
+  }WordFreq;
 
-
-  //output The number of times it appears in the text
-
-
+  //asign str and textFreq from w, so that users can have easier acess to returning values without having acess to WordFreq and Word in the header file
+  typedef struct Word_Data{
+    Word w;
+    char str[];
+    int textFreq;
+    WordFreq aftList[];
+  }Word_Dat;
 
 //Hash bucket table(10 points)
+  
+  //hashtable node
+  typdef struct Node{
+    int key; //not sure abt the type for this 
+    Word_Data value;
+    struct Node* Next;
+  }Node;
+  
+  //linked list
+  typedef struct Bucket{
+    Node* head; 
+  }Bucket;
 
 //Hash Table Struct (5 pts)
+typedef Hash_Table{
+  Bucket** arr;
+  int arrCap; //current capasity of resizing arr
+  int loadFact; //hash table allowed load factor
+}Hash_Table;
+
 
 //Empty Hash Table (5 pts)
 
--- hash_table.h
+++ hash_table.h
@@ -2,10 +2,16 @@
 #define _HASH_TABLE_H_
 
 //Word Data Struct (10 pts)
+//the type that contains data about a word
+typedef struct Word_Data;
 
 //Hash Bucket Struct (10 pts)
+//the type that holds the words that go to the same index
+typedef struct Bucket;
 
 //Hash Table Struct (5 pts)
+//the type of the Hash table itself, stores buckets
+typedef struct Hash_Table;
 
 //Empty Hash Table (5 pts)
 
